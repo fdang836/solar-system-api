0b4ed80ed5d3a422a00e0141a8c3d79629c604fe
Lulu Sun <sunrey32@uw.edu>
2022-04-26 10:57:11 -0700

    finished wave 2

--- app/routes.py
+++ app/routes.py
@@ -26,3 +26,27 @@ def get_all_planets():
             "color": planet.color
         })
     return jsonify(all_planet_list)
+
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def get_one_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        return jsonify({"msg": f"Invalid planet id: {planet_id}"}), 400
+    
+    chosen_planet = None
+    for planet in planets:
+        if planet.id == planet_id:
+            chosen_planet = planet
+            break
+    
+    if chosen_planet is None:
+        return jsonify({"msg": f"Could not find planet id: {planet_id}"}), 404
+
+    rsp = {
+        "id": chosen_planet.id, 
+        "name": chosen_planet.name,
+        "description": chosen_planet.description,
+        "color": chosen_planet.color
+    }
+    return jsonify(rsp), 200
