4cc1987f98784f5e91f4ae2097333977392d0d48
Fena Dang <fenadang@gmail.com>
2022-05-04 10:54:41 -0700

    finished wave 5

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,9 @@ from app import db
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
 def validate_planet(planet_id):
+    """
+    Helper function to validate planet ID
+    """
     try:
         planet_id = int(planet_id)
     except:
@@ -14,11 +17,13 @@ def validate_planet(planet_id):
 
     if not planet:
         abort(make_response({"msg": f"planet {planet_id} not found"}, 404))
-
     return planet
 
 @planets_bp.route('', methods=['POST'])
 def create_one_planet():
+    """
+    Add single planet into database
+    """
     request_body = request.get_json()
     new_planet = Planet(name=request_body['name'], 
                 description=request_body['description'],
@@ -32,6 +37,9 @@ def create_one_planet():
 
 @planets_bp.route('', methods=['GET'])
 def get_all_planet():
+    """
+    Get all planets from database
+    """
     planets = Planet.query.all()
     planets_response = []
     for planet in planets:
@@ -46,6 +54,9 @@ def get_all_planet():
 
 @planets_bp.route('/<planet_id>', methods=['GET'])
 def get_one_planet(planet_id):
+    """
+    Get one planet from database using planet ID
+    """
     planet = validate_planet(planet_id)
     
     rsp = {
@@ -59,6 +70,9 @@ def get_one_planet(planet_id):
 
 @planets_bp.route('/<planet_id>', methods=['PUT'])
 def put_one_planet(planet_id):
+    """
+    Update one planet from database using planet ID
+    """
     chosen_planet = validate_planet(planet_id)
     
     request_body = request.get_json()
@@ -73,6 +87,9 @@ def put_one_planet(planet_id):
 
 @planets_bp.route('/<planet_id>', methods=['DELETE'])
 def delete_one_planet(planet_id):
+    """
+    Delete one planet from database using planet ID
+    """
     chosen_planet = validate_planet(planet_id)
     db.session.delete(chosen_planet)
     db.session.commit()
@@ -80,54 +97,3 @@ def delete_one_planet(planet_id):
         "msg": f"{planet_id} sucessfully deleted"
     }
     return jsonify(rsp), 200
-
-# class Planet:
-#     def __init__(self, id, name, description, color):
-#         self.id = id
-#         self.name = name
-#         self.description = description
-#         self.color = color
-
-# planets = [
-#     Planet(1, "Mercury", "this is mercury", "light grey"),
-#     Planet(2, "Venus", "this is venus", "yellow/white"),
-#     Planet(3, "Earth", "this is earth", "blue/green")
-# ]
-
-
-
-# @planets_bp.route("", methods=["GET"])
-# def get_all_planets():
-#     all_planet_list = []
-#     for planet in planets:
-#         all_planet_list.append({
-#             "id": planet.id, 
-#             "name": planet.name,
-#             "description": planet.description,
-#             "color": planet.color
-#         })
-#     return jsonify(all_planet_list)
-
-# @planets_bp.route("/<planet_id>", methods=["GET"])
-# def get_one_planet(planet_id):
-#     try:
-#         planet_id = int(planet_id)
-#     except:
-#         return jsonify({"msg": f"Invalid planet id: {planet_id}"}), 400
-    
-#     chosen_planet = None
-#     for planet in planets:
-#         if planet.id == planet_id:
-#             chosen_planet = planet
-#             break
-    
-#     if chosen_planet is None:
-#         return jsonify({"msg": f"Could not find planet id: {planet_id}"}), 404
-
-#     rsp = {
-#         "id": chosen_planet.id, 
-#         "name": chosen_planet.name,
-#         "description": chosen_planet.description,
-#         "color": chosen_planet.color
-#     }
-#     return jsonify(rsp), 200
